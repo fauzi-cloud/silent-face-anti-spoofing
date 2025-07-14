8012e8c678759f16e3462df592d95deb79bf9fcc
fauzi-cloud <97300242+fauzi-cloud@users.noreply.github.com>
2025-07-14 17:12:55 +0700

    Delete utils directory

--- utils/visualizer.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_metrics(history):
-    plt.plot(history["loss"], label="Loss")
-    plt.plot(history["accuracy"], label="Accuracy")
-    plt.xlabel("Epoch")
-    plt.legend()
-    plt.show()
